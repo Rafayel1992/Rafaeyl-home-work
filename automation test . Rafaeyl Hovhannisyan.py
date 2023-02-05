9c61e4edfbf981dd7abc4b70d83ce4b60cc76d98
Rafayel1992 <123087885+Rafayel1992@users.noreply.github.com>
2023-02-05 23:50:03 +0400

    hoem work

--- automation test . Rafaeyl Hovhannisyan.py
+++ automation test . Rafaeyl Hovhannisyan.py
@@ -6,40 +6,40 @@ from selenium.webdriver.support import expected_conditions as EC
 import unittest
 import time
 
-class MyTest(unittest.TestCase):
-    def setUp(self):
+
+class MyTest():
+
+    def simon(self):
         self.driver = webdriver.Chrome()
         self.driver.delete_all_cookies()
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.driver.get("https://www.google.com")
 
-
     def test_act(self):
         serchFildElement = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.NAME, "q")))
         serchFildElement.clear()
         serchFildElement.send_keys("BMW  ")
         serchFildElement.send_keys(Keys.ENTER)
-        imgButtonElement =self.driver.find_element(By.XPATH, "(//*[@class='hdtb-mitem'])[1]")
+        imgButtonElement = self.driver.find_element(By.XPATH, "(//*[@class='hdtb-mitem'])[1]")
         imgButtonElement.click()
         time.sleep(2)
         ButtonElement = self.driver.find_element(By.XPATH, "((//*[@class=' bRMDJf islir'])[1])")
         ButtonElement.click()
         time.sleep(5)
+
         try:
-            self.driver.find_element(By.XPATH,"//div[@class='tvh9oe BIB1wf']")
+            self.driver.find_element(By.XPATH, "//div[@class='tvh9oe BIB1wf']")
         except:
-                 print("Errors 5: Iamge not opened")
-                 exit(5)
-
-
+            print("Errors 5: Iamge not opened")
+            exit(5)
 
     def tearDown(self):
 
         self.driver.close()
 
-
-
-
-
-
+if __name__ == "__main__":
+    test = MyTest()
+    test.simon()
+    test.test_act()
+    test.tearDown()
